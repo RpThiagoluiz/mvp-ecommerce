83e5fa27d301adcbc2b183b71a16754a8b14851d
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-12-25 20:08:16 -0300

    feat: footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import { FaInstagram, FaWhatsapp, FaFacebook } from 'react-icons/fa'
+import {socialMedia} from 'whitelabel/test'
+
+export function Footer() {
+    return (
+        <footer className="flex flex-col  w-full p-1  mt-5 bg-pink-100">
+            <Link
+                href={`https://api.whatsapp.com/send?l=pt_br?phone=${socialMedia.WhatsApp}&text=Ol%C3%A1%2C+....`}
+                target="_blank"
+                className="flex flex-row gap-5 items-center card justify-center h-20 bg-base-300"
+            >
+                <FaWhatsapp className="text-[32px] text-green-500" />
+                <h5>Fale conosco!</h5>
+            </Link>
+            <div className="divider">Redes Sociais</div>
+
+            <div className="flex flex-row gap-5 h-32 items-center justify-center bg-inherit">
+                <Link
+                    href={`https://www.instagram.com/${socialMedia.Instagram}`}
+                    target="_blank"
+                >
+                    <FaInstagram className="text-[32px] text-pink-500" />
+                </Link>
+
+                <Link
+                    href={`https://www.facebook.com/${socialMedia.Facebook}`}
+                    target="_blank"
+                >
+                    <FaFacebook className="text-[32px] text-blue-500" />
+                </Link>
+            </div>
+        </footer>
+    )
+}
