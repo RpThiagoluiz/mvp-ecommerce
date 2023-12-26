eb5e5bc4ee0abb86a31ed56ae77d74e985ce8058
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-12-25 21:30:11 -0300

    feat: home with news

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import { Carousel } from './components/Carousel'
+import { FaHeart } from 'react-icons/fa'
 
 export default function Home() {
     return (
         <>
+            <div className="h-20 mt-5 flex flex-row gap-5 items-center justify-center">
+                <FaHeart className="text-pink-500 animate-ping" />
+                <h2 className="text-stone-500 font-semibold">Novidades</h2>
+                <FaHeart className="text-pink-500 animate-ping" />
+            </div>
             <Carousel />
         </>
     )
