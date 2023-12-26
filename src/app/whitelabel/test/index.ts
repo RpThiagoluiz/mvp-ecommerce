93e0e01275be4b60462d070728d44be71eeb64db
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2023-12-25 21:27:03 -0300

    feat: white label assets

--- src/app/whitelabel/test/index.ts
+++ src/app/whitelabel/test/index.ts
@@ -1,5 +1,4 @@
-export const socialMedia = {
-    WhatsApp: '+5562981467685',
-    Facebook: 'thiago.luiz.5621',
-    Instagram: 'thi.luizg',
-} as const
+import { socialMedia } from './socialMedia'
+import Logo from './assets/Logo.png'
+
+export { socialMedia, Logo }
---
+++ src/app/whitelabel/test/socialMedia.ts
@@ -0,0 +1,5 @@
+export const socialMedia = {
+    WhatsApp: '+5562981467685',
+    Facebook: 'thiago.luiz.5621',
+    Instagram: 'thi.luizg',
+} as const
