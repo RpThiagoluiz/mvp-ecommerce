21ef585e45429031684acc65b404ca45badd45f1
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-05 18:18:16 -0300

    feat: adjust footer color

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -4,13 +4,13 @@ import { socialMedia } from 'whitelabel/test'
 
 export function Footer() {
     return (
-        <footer className="flex flex-col  w-full p-1 pt-5 mt-5 bg-cyan-50">
+        <footer className="flex flex-col  w-full p-1 pt-5 mt-5 bg-zinc-100">
             <Link
                 href={`https://api.whatsapp.com/send?l=pt_br?phone=${socialMedia.WhatsApp}&text=Ol%C3%A1%2C+....`}
                 target="_blank"
-                className="flex flex-row gap-5 items-center card justify-center h-20 bg-base-300"
+                className="mb-5 flex flex-row gap-5 items-center justify-center h-20 border border-green-500 rounded w-1/3 self-center text-green-500 hover:bg-green-500 hover:text-white hover:scale-110 transition-all ease-in-out delay-350"
             >
-                <FaWhatsapp className="text-[32px] text-green-500" />
+                <FaWhatsapp className="text-[32px]" />
                 <h5>Fale conosco!</h5>
             </Link>
             <div className="divider text-gray-500">Redes Sociais</div>
