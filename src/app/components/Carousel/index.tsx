c043b0246a626b0bfc9489616b683963f7d75e28
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-05 18:17:46 -0300

    feat: adjust carousel colors and size

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -5,17 +5,25 @@ import Image, { type StaticImageData } from 'next/image'
 import { useCarousel } from './hook/useCarousel'
 import React from 'react'
 
-export function Carousel({ slides }: { slides: StaticImageData[] }) {
+export function Carousel({
+    slides,
+    extraStyles,
+}: {
+    slides: StaticImageData[]
+    extraStyles?: React.ComponentProps<'div'>['className']
+}) {
     const carousel = useCarousel()
 
     return (
-        <div className="overflow-hidden relative flex justify-center align-center  w-full  p-2">
+        <div
+            className={`overflow-hidden relative flex justify-center align-center w-full  ${extraStyles}`}
+        >
             <div className={`flex transition ease-out duration-40`}>
                 <Image
                     width={360}
                     height={360}
                     src={slides[carousel.currentImage]}
-                    className="lg:h-[960px] sm:h-96  w-full 
+                    className="lg:h-[640px] sm:h-96 w-full 
                      rounded-box"
                     alt=""
                     onClick={() =>
@@ -24,16 +32,22 @@ export function Carousel({ slides }: { slides: StaticImageData[] }) {
                 />
             </div>
 
-            <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-10 text-3xl">
-                <button onClick={carousel.previous} className='text-[#FCA311]'>
+            <div className="absolute top-0 h-full w-5/6 justify-between items-center flex text-2xl">
+                <button
+                    onClick={carousel.previous}
+                    className="text-white bg-zinc-200 rounded-full opacity-65 p-3"
+                >
                     <FaChevronLeft />
                 </button>
-                <button onClick={carousel.next} className='text-[#FCA311]'>
+                <button
+                    onClick={carousel.next}
+                    className="text-white bg-zinc-200 rounded-full opacity-65 p-3"
+                >
                     <FaChevronRight />
                 </button>
             </div>
 
-            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full opacity-65">
                 {slides.map((_, index) => {
                     return (
                         <div
@@ -43,7 +57,7 @@ export function Carousel({ slides }: { slides: StaticImageData[] }) {
                             key={'circle' + index}
                             className={`rounded-full w-5 h-5 cursor-pointer  ${
                                 index == carousel.currentImage
-                                    ? 'bg-[#FCA311]'
+                                    ? 'bg-green-500'
                                     : 'bg-gray-500'
                             }`}
                         ></div>
