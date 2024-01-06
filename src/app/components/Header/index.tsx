dcfb36065800f297d167b1a9b40f6531a25fdcbd
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-06 10:30:49 -0300

    fix: styles

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,10 +6,8 @@ type HeaderProps = {
 
 export function Header({ text }: HeaderProps) {
     return (
-        <div className="w-full">
-            <h2 className="text-3xl md:text-5xl font-bold text-center bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-                {text}
-            </h2>
+        <div className="text-center bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text ">
+            <h2 className="text-3xl md:text-5xl font-bold ">{text}</h2>
         </div>
     )
 }
