ee9cab7629e18cedfaa3addf1c7bf9a10ab7b4e4
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-01-07 16:27:10 -0300

    chore: mock images

---
+++ src/app/whitelabel/test/assets/index.ts
@@ -0,0 +1,16 @@
+import photoOne from './photos/photoOne.jpg'
+import photoTwo from './photos/photoTwo.jpg'
+import photoThree from './photos/photoThree.jpg'
+
+import pantsMale from './products/pantsMale/teste_01.jpg'
+import pantsMaleTwo from './products/pantsMaleModel2/hero.jpg'
+import pantsFemale from './products/pantsFemale/hero.jpg'
+import pantsFemaleTwo from './products/pantsFemaleTwo/hero.jpg'
+
+export const photos = {
+    photoOne,
+    photoTwo,
+    photoThree,
+}
+
+export const products = [pantsMale, pantsMaleTwo, pantsFemale, pantsFemaleTwo]
--- src/app/whitelabel/test/index.ts
+++ src/app/whitelabel/test/index.ts
@@ -1,5 +1,6 @@
 import { socialMedia } from './socialMedia'
 import Logo from './assets/Logo.png'
 import { highLightImages } from './assets/highLight'
+import { photos } from './assets'
 
-export { socialMedia, Logo, highLightImages }
+export { socialMedia, Logo, highLightImages, photos }
